c648f981988ff3d247fecd3d761218bb2fb16e91
DVKevin <martins.kevin.kk@gmail.com>
2025-11-20 23:22:44 -0300

    feat: criando a page integrantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
 import { Inicial } from "./pages/home";
+import { Integrantes } from "./pages/Integrantes";
+
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>          
           <Route index element={<Inicial />} />
+          {/* Footer */}
+          <Route path="/integrantes" element={<Integrantes />} />
         </Route>
       </Routes>
     </BrowserRouter>
