3e8500e94462789c6edf6729735bf2121f4dd07c
DVKevin <martins.kevin.kk@gmail.com>
2025-11-20 22:10:05 -0300

    feat: criando a home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layout";
+import { Inicial } from "./pages/home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>          
+          <Route index element={<Inicial />} />
         </Route>
       </Routes>
     </BrowserRouter>
